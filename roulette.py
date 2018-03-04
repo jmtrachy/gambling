e1846153ac1a784dfe43ff635ead9092328bb4c1
James Trachy <james.trachy@concur.com>
2018-03-03 22:22:10 -0600

    Modified all double quotes out fo the system and changed prints to use .format instead of silly string concatenation

--- roulette.py
+++ roulette.py
@@ -19,17 +19,22 @@ class Roulette:
         return returned_value
 
     @staticmethod
-    def play(strategy, num_trips = 100000):
+    def play(strategy, num_trips=100000):
         trips_to_tl = []
-        print("Heading to the casino")
+        print('Heading to the casino')
+        num_trips_counter = 0
         for i in range(0, num_trips):
             trips_to_tl.append(Roulette.__head_to_tl(strategy, num_trips))
-        print("Returning from the casino")
+
+            # Silly little printing
+            num_trips_counter += 1
+            if num_trips_counter == 10000:
+                print('.', end='', flush=True)
+                num_trips_counter = 0
+        print('\nReturning from the casino')
 
         ranges = Roulette.create_ranges()
-        print("initializing return maps")
         percent_returns = Roulette.initialize_returns_map(ranges)
-        print("return maps initialized")
 
         # start of non-checked changes
         max_winnings = 0
@@ -38,7 +43,7 @@ class Roulette:
         total_time_at_tl = 0
         total_times_max_win = 0
 
-        print("putting results in buckets")
+        print('putting results in buckets')
         for trip_to_tl in trips_to_tl:
             percent_return = trip_to_tl.result / float(strategy.starting_balance)
             if trip_to_tl.result > max_winnings:
@@ -57,7 +62,7 @@ class Roulette:
         chances_of_doubling = 0
         chances_of_winning = 0
 
-        print("calculating aggregate totals")
+        print('')
         for r in ranges:
             percent_of_pot = percent_returns[r.label] / float(num_trips)
             if r.min < .9:
@@ -66,22 +71,22 @@ class Roulette:
                 chances_of_winning += percent_of_pot
             if r.min >= 2:
                 chances_of_doubling += percent_of_pot
-            if r.label == "-10+10%":
+            if r.label == '-10+10%':
                 chances_of_breaking_even = percent_of_pot
             if percent_of_pot > 0:
-                print(r.label + " = " + str(percent_returns[r.label]) + "; {:.2%}".format(percent_of_pot))
+                print('{} = {}; {:.2%}'.format(r.label, percent_returns[r.label], percent_of_pot))
+                #print(r.label + ' = ' + str(percent_returns[r.label]) + '; {:.2%}'.format(percent_of_pot))
 
-        print("")
-        print("Best trip to Turtle lake results in: " + str(max_winnings) + " - which happened " + str(total_times_max_win) + " times")
-        print("")
-        print("Chances of losing money: {:.2%}".format(chances_of_losing_money))
-        print("Chances of breaking even: {:.2%}".format(chances_of_breaking_even))
-        print("Chances of winning: {:.2%}".format(chances_of_winning))
-        print("Chances of doubling: {:.2%}".format(chances_of_doubling))
-        print("Total time at TL = " + str(total_time_at_tl))
-        print("Average time at casino: " + str(format(total_time_at_tl / float(num_trips))))
-        if total_omgs > 0:
-            print("Average OMG Moments: {:.2}".format(total_omgs / float(num_trips)) + " with an average OMG payout of $" + str(sum_of_omgs / float(total_omgs)))
+        print('\nBest trip to casino results in: {} - which happened {} times'.format(max_winnings, total_times_max_win))
+        print('\nChances of losing money: {:.2%}'.format(chances_of_losing_money))
+        print('Chances of breaking even: {:.2%}'.format(chances_of_breaking_even))
+        print('Chances of winning: {:.2%}'.format(chances_of_winning))
+        print('Chances of doubling: {:.2%}'.format(chances_of_doubling))
+        # print('Total time at TL = ' + str(total_time_at_tl))
+        # print('Average time at casino: ' + str(format(total_time_at_tl / float(num_trips))))
+        # if total_omgs > 0:
+        #    print('Average OMG Moments: {:.2}'.format(total_omgs / float(num_trips)) +
+        #          ' with an average OMG payout of $' + str(sum_of_omgs / float(total_omgs)))
 
         return RouletteStrategyResult(trips_to_tl, max_winnings)
 
@@ -119,47 +124,47 @@ class Roulette:
         bucketed = False
         for r in all_ranges:
             if r.min <= result < r.max:
-                pct_returns[r.label] = pct_returns[r.label] + 1
+                pct_returns[r.label] += 1
                 bucketed = True
         if not bucketed:
-            print("Did not bucket: " + str(result))
+            print('Did not bucket: {}'.format(result))
 
     @staticmethod
     def create_ranges():
         ranges = []
-        ranges.append(Range("-100-90%", 0, .1))
-        ranges.append(Range("-90-80%", .1, .2))
-        ranges.append(Range("-80-70%", .2, .3))
-        ranges.append(Range("-70-60%", .3, .4))
-        ranges.append(Range("-60-50%", .4, .5))
-        ranges.append(Range("-50-40%", .5, .6))
-        ranges.append(Range("-40-30%", .6, .7))
-        ranges.append(Range("-30-20%", .7, .8))
-        ranges.append(Range("-20-10%", .8, .9))
-        ranges.append(Range("-10+10%", .9, 1.1))
-        ranges.append(Range("10-20%", 1.1, 1.2))
-        ranges.append(Range("20-30%", 1.2, 1.3))
-        ranges.append(Range("30-40%", 1.3, 1.4))
-        ranges.append(Range("40-50%", 1.4, 1.5))
-        ranges.append(Range("50-60%", 1.5, 1.6))
-        ranges.append(Range("60-70%", 1.6, 1.7))
-        ranges.append(Range("70-80%", 1.7, 1.8))
-        ranges.append(Range("80-90%", 1.8, 1.9))
-        ranges.append(Range("90-100%", 1.9, 2))
-        ranges.append(Range("100-120%", 2, 2.2))
-        ranges.append(Range("120-150%", 2.2, 2.5))
-        ranges.append(Range("150-200%", 2.5, 3))
-        ranges.append(Range("200-300%", 3, 4))
-        ranges.append(Range("300-400%", 4, 5))
-        ranges.append(Range("400-500%", 5, 6))
-        ranges.append(Range("500%+", 6, 7000))
+        ranges.append(Range('-100-90%', 0, .1))
+        ranges.append(Range('-90-80%', .1, .2))
+        ranges.append(Range('-80-70%', .2, .3))
+        ranges.append(Range('-70-60%', .3, .4))
+        ranges.append(Range('-60-50%', .4, .5))
+        ranges.append(Range('-50-40%', .5, .6))
+        ranges.append(Range('-40-30%', .6, .7))
+        ranges.append(Range('-30-20%', .7, .8))
+        ranges.append(Range('-20-10%', .8, .9))
+        ranges.append(Range('-10+10%', .9, 1.1))
+        ranges.append(Range('10-20%', 1.1, 1.2))
+        ranges.append(Range('20-30%', 1.2, 1.3))
+        ranges.append(Range('30-40%', 1.3, 1.4))
+        ranges.append(Range('40-50%', 1.4, 1.5))
+        ranges.append(Range('50-60%', 1.5, 1.6))
+        ranges.append(Range('60-70%', 1.6, 1.7))
+        ranges.append(Range('70-80%', 1.7, 1.8))
+        ranges.append(Range('80-90%', 1.8, 1.9))
+        ranges.append(Range('90-100%', 1.9, 2))
+        ranges.append(Range('100-120%', 2, 2.2))
+        ranges.append(Range('120-150%', 2.2, 2.5))
+        ranges.append(Range('150-200%', 2.5, 3))
+        ranges.append(Range('200-300%', 3, 4))
+        ranges.append(Range('300-400%', 4, 5))
+        ranges.append(Range('400-500%', 5, 6))
+        ranges.append(Range('500%+', 6, 7000))
 
         # temporary ranges for $20 17 bets
-        #ranges.append(Range("single hit", 5, 10))
-        #ranges.append(Range("double hit", 10, 16))
-        #ranges.append(Range("triple hit", 16, 25))
-        #ranges.append(Range("quads!!!", 25, 30))
-        #ranges.append(Range("ALL FIVE", 30, 50))
+        #ranges.append(Range('single hit', 5, 10))
+        #ranges.append(Range('double hit', 10, 16))
+        #ranges.append(Range('triple hit', 16, 25))
+        #ranges.append(Range('quads!!!', 25, 30))
+        #ranges.append(Range('ALL FIVE', 30, 50))
 
         return ranges
 
@@ -188,7 +193,7 @@ class RouletteStrategy:
         for outer_bet in self.outside_bets:
             total_dps += outer_bet.num_bets
 
-        print("Each spin costs $" + str(total_dps))
+        print('Each spin costs ${}'.format(total_dps))
         return total_dps
 
     def generate_inside_bet_map(self):
@@ -206,7 +211,7 @@ class RouletteStrategy:
         inside_bet_returns = {}
         for num in range(1, 39):
             inside_bet_returns[num] = self.compute_inside_spin_return(num)
-            print("Return for spinning a " + str(num) + " = " + str(self.compute_inside_spin_return(num)))
+            print('Return for spinning a {} = {}'.format(num, self.compute_inside_spin_return(num)))
 
         return inside_bet_returns
 
@@ -237,7 +242,8 @@ class Bet:
     __metaclass__ = ABCMeta
 
     @abstractmethod
-    def compute_bet_return(self): pass
+    def compute_bet_return(self):
+        pass
 
 
 class OutsideBet(Bet):
@@ -293,9 +299,9 @@ def add_bets_to_single_number(num_bets, number, bet_collection):
         bet_collection.append(InsideBet([number]))
 
 
-#def add_bets_to_two_numbers(num_bets)
+# def add_bets_to_two_numbers(num_bets)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     start_millis = current_time_millis()
 
     inners = []
@@ -306,19 +312,17 @@ if __name__ == "__main__":
     # $20 on 20
     add_bets_to_single_number(bet_size, 26, inners)
     # $20 on 20
-    #add_bets_to_single_number(bet_size, 5, inners)
+    # add_bets_to_single_number(bet_size, 5, inners)
     # $20 on 20
-    #add_bets_to_single_number(bet_size, 14, inners)
-
+    # add_bets_to_single_number(bet_size, 14, inners)
 
     # inners.extend([InsideBet([17]), InsideBet([16, 17, 13, 14]), InsideBet([17, 18, 14, 15]), InsideBet([17, 20, 16, 19]), InsideBet([17, 20, 18, 21])])
     # inners.extend([InsideBet([22, 23, 25, 26]), InsideBet([23, 24, 26, 27]), InsideBet([25, 26, 28, 29]), InsideBet([26, 27, 29, 30]), InsideBet([26])])
     # inners.extend([InsideBet([26]), InsideBet([22, 23, 25, 26]), InsideBet([23, 24, 26, 27]), InsideBet([25, 26, 28, 29]), InsideBet([26, 27, 29, 30]), InsideBet([26, 23]), InsideBet([26, 25]), InsideBet([26, 27]), InsideBet([26, 29])])
-
 
     outers = []
     rs = RouletteStrategy(inners, outers, 4, 400)
     Roulette.play(rs, 1000000)
 
     end_millis = current_time_millis()
-    print("Execution took " + str(end_millis - start_millis) + " milliseconds.")
+    print('Execution took {} milliseconds.')
